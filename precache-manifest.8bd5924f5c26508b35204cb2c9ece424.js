a05d1efa98f1dbd79a3f260e38ea4f8f02754108
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-28 12:09:09 +0100

    Updates

---
+++ precache-manifest.8bd5924f5c26508b35204cb2c9ece424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d8d23daf5b387e0b0fc958070a88c8c",
+    "url": "/instagram_clone/index.html"
+  },
+  {
+    "revision": "142ab367aeb1480abff0",
+    "url": "/instagram_clone/static/css/main.6a758cf9.chunk.css"
+  },
+  {
+    "revision": "7a4a23591a8e7d95cdf1",
+    "url": "/instagram_clone/static/js/2.63cd5bd9.chunk.js"
+  },
+  {
+    "revision": "142ab367aeb1480abff0",
+    "url": "/instagram_clone/static/js/main.b7d32942.chunk.js"
+  },
+  {
+    "revision": "ceef2ad5b5f2ce5f7c78",
+    "url": "/instagram_clone/static/js/runtime~main.db7962f8.js"
+  },
+  {
+    "revision": "dcc389e54c4374fb8ccf2e8fc2ecedf9",
+    "url": "/instagram_clone/static/media/instagram-logo.dcc389e5.png"
+  }
+]);
